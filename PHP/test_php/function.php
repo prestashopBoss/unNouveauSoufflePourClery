034b5222a7fd9267908482be4447eed84dd530c3
Wakidaiho <wakidaisho.jmalezieux@gmail.com>
2021-05-10 00:50:21 +0200

    php_test

--- PHP/test_php/function.php
+++ PHP/test_php/function.php
@@ -2,3 +2,14 @@
 $phrase = 'ceci est une phrase';
 $nbr = strlen($phrase);
 echo 'Il y a ' . $nbr . ' charactères dans la phrase ' . $phrase;
+?>
+</br>
+<?php
+echo 'Nous sommes le ' . date('l, d, F,') . 'semaine N° ' . date('w') . ' Nous sommes le ' . date('z') . 'eme jour de l\'année';
+
+function pourcentage($val1, $val2){
+    $result = ($val1 * $val2) / 100;
+    return '</br>' . $result . ' %';
+}
+
+echo pourcentage(100, 20);
