b137075dbb2e0dcbe5e6635fb50753f09a668d78
prestashopBoss <wakidaisho@protonmail.com>
2022-01-31 10:52:10 +0100

    test modal3

--- connect.php
+++ connect.php
@@ -12,29 +12,10 @@ if (isset($_POST['valider'])) {
   $mdp_saisi = htmlspecialchars($_POST['mdp']);
   if ($pseudo_saisi == $pseudo && $mdp_saisi = $mdp){
     $_SESSION['mdp'] = $mdp_saisi;
-
     
-
 }else{
-  echo '<div class="modal" tabindex="-1" role="dialog">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <p>Modal body text goes here.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary">Save changes</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>';
+  echo "NOK";
+  echo "<script>window.alert('Veuillez remplir les champs correctement')</script>";
 }
 } 
 }
